3fa2a5c3a9a89f3fa696e949585b103f2e192694
Daniel Pawlak <daniel@Daniels-iMac.local>
2021-02-28 00:38:12 -0800

    Fetching a Single Post

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const feedRoutes = require('./routes/feed')
 
 const app = express()
 
 app.use(bodyParser.json())
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
@@ -16,4 +20,17 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes)
 
-app.listen(8000)
+app.use((err, req, res, next) => {
+     console.log(error)
+     const status = error.statusCode || 500
+     const message = error.message
+     res.status(status).json({mesage: message})
+})
+
+mongoose.connect(
+        // Insert mongodb here...
+    )
+    .then(result => {
+        app.listen(8000)
+    })
+    .catch(err => console.log(err))
