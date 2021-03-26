017dfff35f501aa3990955ef910401644c42a0ea
Daniel Pawlak <daniel@Daniels-iMac.local>
2021-03-26 12:20:46 -0700

    GraphQL Integration Backend, Sending the Create Post Query.

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const multer = require('multer')
-const cors = require('cors')
-
-const feedRoutes = require('./routes/feed')
-const authRoutes = require('./routes/auth')
+const  {graphqlHTTP } = require('express-graphql')
+const graphqlSchema = require('./graphql/schema')
+const graphqlResolver = require('./graphql/resolvers')
+const auth = require('./middleware/auth')
 
 const app = express()
 
@@ -26,7 +26,7 @@ const fileFilter = (req, file, cb) => {
         cb(null, false)
     }
 }
-app.use(cors());
+
 app.use(bodyParser.json())
 app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
 app.use('/images', express.static(path.join(__dirname, 'images')))
@@ -35,11 +35,31 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization' )
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200)
+    }
     next()
 })
 
-app.use('/feed', feedRoutes)
-app.use('/auth', authRoutes)
+app.use(auth)
+
+app.use(
+    '/graphql', 
+    graphqlHTTP({
+        schema: graphqlSchema,
+        rootValue: graphqlResolver,
+        graphiql: true,
+        formatError(err) {
+            if(!err.originalError) {
+                return err;
+            }
+            const data = err.originalError.data
+            const message = err.message || 'An error occured.'
+            const code = err.originalError.code || 500
+            return { message: message, status: code, data: data }
+        }
+    })
+)
 
 app.use((error, req, res, next) => {
      console.log(error)
@@ -54,10 +74,6 @@ app.use((error, req, res, next) => {
       'mongodb+srv://Daniel:test123@cluster0.dizsj.mongodb.net/messages?retryWrites=true&w=majority'
     )
     .then(result => {
-      const server = app.listen(8000);
-      const io = require('./socket').init(server);
-      io.on('connection', socket => {
-        console.log('Client connected');
-      });
+      app.listen(8000);
     })
     .catch(err => console.log(err));
